f2642340ffb520a1d7bd074eab678016239b88cb
Emkat <efurniss@salesforce.com>
2021-08-18 12:36:16 +0200

    changed default city

--- src/app.js
+++ src/app.js
@@ -82,4 +82,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("New York");
+search("Cologne");
