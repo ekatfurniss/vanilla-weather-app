1ffd9b55bdc9907798ca98e22c2776bae0e01339
Emkat <efurniss@salesforce.com>
2021-08-24 15:12:41 +0200

    added dynamic html to create further forecasts

--- src/app.js
+++ src/app.js
@@ -21,6 +21,34 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+        <div class="col-2">
+            <div class="forecast-date">
+            ${day}     
+            </div>
+            <img src="http://openweathermap.org/img/wn/01d@2x.png"
+            alt=""
+            width="42" 
+            />
+            <div class="forecast-temperature">
+            <span class="forecast-temperature-max">18°</span> 
+            <span class="forecast-temperature-min">12°</span>
+            </div>
+        </div>
+        `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -83,3 +111,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Cologne");
+displayForecast();
