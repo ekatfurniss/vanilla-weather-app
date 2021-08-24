dba3fb339aabfa665c587a0b56902ed0567e6eab
Emkat <efurniss@salesforce.com>
2021-08-24 16:52:33 +0200

    display forecast data using the API

--- src/app.js
+++ src/app.js
@@ -21,29 +21,45 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 7 && index > 0) {
+      forecastHTML =
+        forecastHTML +
+        `
         <div class="col-2">
             <div class="forecast-date">
-            ${day}     
+            ${formatDay(forecastDay.dt)}     
             </div>
-            <img src="http://openweathermap.org/img/wn/01d@2x.png"
+            <img src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"
             alt=""
             width="42" 
             />
             <div class="forecast-temperature">
-            <span class="forecast-temperature-max">18°</span> 
-            <span class="forecast-temperature-min">12°</span>
+            <span class="forecast-temperature-max">${Math.round(
+              forecastDay.temp.max
+            )}°</span> 
+            <span class="forecast-temperature-min">${Math.round(
+              forecastDay.temp.min
+            )}°</span>
             </div>
         </div>
         `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
